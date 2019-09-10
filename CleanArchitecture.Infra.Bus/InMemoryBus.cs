9009b25d6c41d0d1117b3953257002dd2abc3481
LEANDRO-PC\Leandro <leandromtr@hotmail.com>
2019-09-10 15:22:39 +0100

    Added DomainCore and Implementationod in MemoryBUS

--- CleanArchitecture.Infra.Bus/InMemoryBus.cs
+++ CleanArchitecture.Infra.Bus/InMemoryBus.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using CleanArchitecture.Domain.Core.Bus;
+using CleanArchitecture.Domain.Core.Commands;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CleanArchitecture.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus: IMediatorHandler
     {
+        private readonly IMediator _mediator;
 
+        public InMemoryBus(IMediator mediator)
+        {
+            mediator = _mediator;
+        }
+
+        public Task SendCommand<T>(T command) where T: Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
